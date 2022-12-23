8d0460ab78dfc4f44580631798242162c1dcf8bf
Reinhard Merfeld <reinhard@merfeld.info>
2022-12-23 20:59:22 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Reinhard Merfeld on 23.12.22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
