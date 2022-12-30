631b34c87ca60104a227381027bb97a6b396fef6
Reinhard Merfeld <reinhard@merfeld.info>
2022-12-30 19:11:57 +0100

    appIcon and beginning of Badge

---
+++ Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,66 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Reinhard Merfeld on 30.12.22.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    
+    static let gradientStartColor = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+    static let gradientEndColor = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
+    static let gradientStartPoint = UnitPoint(x: 0.5, y: 0)
+    static let gradientEndPoint = UnitPoint(x: 0.5, y: 0.6)
+    
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95 + xOffset,
+                        y: height * (0.20 + HexagonParameters.adjustment)
+                    )
+                )
+                
+                HexagonParameters.segments.forEach { segment in
+                    path.addLine(
+                        to: CGPoint(
+                            x: width * segment.line.x + xOffset,
+                            y: height * segment.line.y
+                        )
+                    )
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x + xOffset,
+                            y: height * segment.curve.y
+                        ),
+                        control:  CGPoint(
+                            x: width * segment.control.x + xOffset,
+                            y: height * segment.control.y
+                        )
+                    )
+                }
+            }
+            .fill(.linearGradient(
+                Gradient(colors: [Self.gradientStartColor, Self.gradientEndColor]),
+                startPoint: Self.gradientStartPoint,
+                endPoint: Self.gradientEndPoint
+            ))
+        }
+        .aspectRatio(1, contentMode: .fit)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
