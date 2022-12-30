22b36c276a63743257454e78e3231e560cb9f5fa
Reinhard Merfeld <reinhard@merfeld.info>
2022-12-30 20:03:42 +0100

    Badge finished

---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Reinhard Merfeld on 30.12.22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol()
+    }
+}
